14515388d7dc81808951f468ea3d2a53ec13f5b0
Johnson Joseph <jhonsonj88@gmail.com>
2019-06-16 21:12:16 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
 end
+
+#create - We need to draw a create route so that the routing system knows what to do when a POST request is sent to the /posts resource:
